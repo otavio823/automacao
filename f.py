eacedf8ea0505e6a43e2716d100cb26415d60d64
sanziolopes <108752809+sanziolopes@users.noreply.github.com>
2023-09-22 10:34:16 -0300

    alterações - sanzio

--- f.py
+++ f.py
@@ -9,15 +9,10 @@ CPF = '28011724649'
 driver = webdriver.Chrome()
 driver.get('https://siatu-tributario.pbh.gov.br/guias/iptu')
 
-n = 0
-while n == 0:
-    try:
-        driver.find_element(By.CSS_SELECTOR,'[id="cep"]').send_keys(CEP)
-        time.sleep(2)
-        driver.find_element(By.CSS_SELECTOR,'[id="cpfCnpj"]').send_keys(CPF,Keys.ENTER)
-        time.sleep(5)
-        driver.find_element(By.CSS_SELECTOR,'[class="btn ml-3 mr-3 float-right btn-primary"]').click()
-        n = 1
-        input('wait')
-    except:
-        time.sleep(1)
+driver.find_element(By.CSS_SELECTOR,'[id="cep"]').send_keys(CEP)
+time.sleep(2)
+driver.find_element(By.CSS_SELECTOR,'[id="cpfCnpj"]').send_keys(CPF,Keys.ENTER)
+time.sleep(5)
+driver.find_element(By.CSS_SELECTOR,'[class="btn ml-3 mr-3 float-right btn-primary"]').click()
+n = 1
+input('wait')
